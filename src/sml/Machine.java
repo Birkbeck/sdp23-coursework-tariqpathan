68e0d8541e442f1ce1dad16382be69e3b526944f
Tariq Pathan <tariq047@gmail.com>
2023-02-16 17:37:37 +0000

    Update: Machine.java
    Update equals to use pattern matching - to verify if correct

--- src/sml/Machine.java
+++ src/sml/Machine.java
@@ -74,9 +74,9 @@ public final class Machine {
 	// https://docs.oracle.com/en/java/javase/14/language/pattern-matching-instanceof-operator.html
 	@Override
 	public boolean equals(Object o) {
-		if (o instanceof Machine) {
+		if (o instanceof Machine other) {
 			// TODO:
-			Machine other = (Machine) o;
+			//Machine other = (Machine) o;
 			return Objects.equals(this.labels, other.labels)
 					&& Objects.equals(this.program, other.program)
 					&& Objects.equals(this.registers, other.registers)
--- src/sml/instruction/SubInstruction.java
+++ src/sml/instruction/SubInstruction.java
@@ -4,21 +4,19 @@ import sml.Instruction;
 import sml.Machine;
 import sml.RegisterName;
 
-import java.util.Objects;
-
 // TODO: write a JavaDoc for the class
 
 /**
  * @author
  */
 
-public class AddInstruction extends Instruction {
+public class SubInstruction extends Instruction {
 	private final RegisterName result;
 	private final RegisterName source;
 
-	public static final String OP_CODE = "add";
+	public static final String OP_CODE = "sub";
 
-	public AddInstruction(String label, RegisterName result, RegisterName source) {
+	public SubInstruction(String label, RegisterName result, RegisterName source) {
 		super(label, OP_CODE);
 		this.result = result;
 		this.source = source;
@@ -28,7 +26,7 @@ public class AddInstruction extends Instruction {
 	public int execute(Machine m) {
 		int value1 = m.getRegisters().get(result);
 		int value2 = m.getRegisters().get(source);
-		m.getRegisters().set(result, value1 + value2);
+		m.getRegisters().set(result, value1 - value2);
 		return NORMAL_PROGRAM_COUNTER_UPDATE;
 	}
 
@@ -39,11 +37,13 @@ public class AddInstruction extends Instruction {
 
 	@Override
 	public boolean equals(Object obj) {
+		// TODO
 		return false;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(result, source);
+		// TODO
+		return 0;
 	}
 }
