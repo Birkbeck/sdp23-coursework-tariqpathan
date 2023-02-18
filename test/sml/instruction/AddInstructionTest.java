94f1b4b762dba44e4e45ef2f6aacf27a39b15085
Tariq Pathan <tariq047@gmail.com>
2023-02-18 22:30:57 +0000

    Update: AddInstructionTest
    Added more tests for AddInstruction class, this includes testing the correct label is set, the OP_CODE is valid, the toString method (with and without labels), the return value returns -1, and the equals and hashCode methods work

--- test/sml/instruction/AddInstructionTest.java
+++ test/sml/instruction/AddInstructionTest.java
@@ -1,9 +1,6 @@
 package sml.instruction;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import sml.Instruction;
 import sml.Machine;
@@ -28,6 +25,26 @@ class AddInstructionTest {
     registers = null;
   }
 
+  @Nested
+  class AddInstructionConstructor {
+    private Instruction instruction;
+
+    @BeforeEach
+    void setUp() {
+      instruction = new AddInstruction("aLabel", EAX, EBX);
+    }
+
+    @Test
+    void labelValid() {
+      Assertions.assertEquals("aLabel", instruction.getLabel());
+    }
+
+    @Test
+    void OPCodeValid() {
+      Assertions.assertEquals("add", instruction.getOpcode());
+    }
+  }
+
   @Test
   void executeValid() {
     registers.set(EAX, 5);
@@ -47,7 +64,7 @@ class AddInstructionTest {
   }
 
   @Test
-  void toStringValidOne() {
+  void toStringWithoutLabelValid() {
     registers.set(EAX, -5);
     registers.set(EBX, 6);
     Instruction instruction = new AddInstruction(null, EAX, EBX);
@@ -56,11 +73,107 @@ class AddInstructionTest {
   }
 
   @Test
-  void toStringValidTwo() {
-    registers.set(EAX, -5);
-    registers.set(EBX, 6);
-    Instruction instruction = new AddInstruction(null, EAX, EBX);
+  void toStringWithLabelValid() {
+    registers.set(ECX, 4);
+    registers.set(ESI, 2);
+    Instruction instruction = new AddInstruction("addOne", ECX, ESI);
     instruction.toString();
-    Assertions.assertEquals("add EAX EBX", instruction.toString());
+    Assertions.assertEquals("addOne: add ECX ESI", instruction.toString());
+  }
+
+  @Test
+  void returnValueValid() {
+    registers.set(EAX, 3);
+    registers.set(EBP, 2);
+    Instruction instruction = new AddInstruction(null, EAX, EBP);
+    int returnValue = instruction.execute(machine);
+    Assertions.assertEquals(-1, returnValue);
+  }
+
+  @Nested
+  class TestTwoInstructions {
+    private Instruction instructionOne;
+
+    @BeforeEach
+    void setup() {
+      registers.set(EAX, 1);
+      registers.set(EBX, 2);
+      registers.set(ECX, 3);
+      instructionOne = new AddInstruction("aLabel", EAX, EBX);
+    }
+
+    @Test
+    void equalsTrueWithSameInstructionObject() {
+      Assertions.assertTrue(instructionOne.equals(instructionOne));
+    }
+
+    @Test
+    void equalsTrueWithSameValues() {
+      Instruction instructionTwo = new AddInstruction("aLabel", EAX, EBX);
+
+      Assertions.assertTrue(instructionOne.equals(instructionTwo));
+      Assertions.assertTrue(instructionTwo.equals(instructionOne));
+    }
+
+    @Test
+    void equalsFalseWithRegisterChange() {
+      Instruction instructionThree = new AddInstruction("aLabel", EAX, ECX);
+
+      Assertions.assertFalse(instructionOne.equals(instructionThree));
+      Assertions.assertFalse(instructionThree.equals(instructionOne));
+    }
+
+    @Test
+    void equalsFalseWithLabelChange() {
+      Instruction instructionThree = new AddInstruction("aLabel", EAX, ECX);
+      Instruction instructionFour = new AddInstruction(null, EAX, EBX);
+      Assertions.assertFalse(instructionThree.equals(instructionFour));
+      Assertions.assertFalse(instructionFour.equals(instructionThree));
+    }
+
+    @Test
+    void equalsFalseWithNull() {
+      Assertions.assertFalse(instructionOne.equals(null));
+    }
+
+    @Test
+    void equalsFalseWithObject() {
+      Object object = new Object();
+      Assertions.assertFalse(instructionOne.equals(object));
+    }
+
+    @Test
+    void hashCodeForSameInstructionValid() {
+      Assertions.assertEquals(instructionOne.hashCode(), instructionOne.hashCode());
+    }
+
+    @Test
+    void hashCodeForEqualValuesValid() {
+      Instruction instructionTwo = new AddInstruction("aLabel", EAX, EBX);
+      Assertions.assertEquals(instructionOne.hashCode(), instructionTwo.hashCode());
+    }
+
+    @Test
+    void hashCodeNotEqualWithRegisterChangeValid() {
+      Instruction instructionThree = new AddInstruction("aLabel", EAX, ECX);
+      Assertions.assertNotEquals(instructionOne.hashCode(), instructionThree.hashCode());
+    }
+
+    @Test
+    void hashCodeNotEqualWithLabelChangeValid() {
+      Instruction instructionFour = new AddInstruction(null, EAX, EBX);
+      Assertions.assertNotEquals(instructionOne.hashCode(), instructionFour.hashCode());
+    }
+
+    @Test
+    void hashCodeNotEqualWithNullValid() {
+      Assertions.assertNotEquals((Integer) null, instructionOne.hashCode());
+    }
+
+    @Test
+    void hashCodeNotEqualWithObjectValid() {
+      Object object = new Object();
+      Assertions.assertNotEquals(object.hashCode(), instructionOne.hashCode());
+    }
   }
 }
