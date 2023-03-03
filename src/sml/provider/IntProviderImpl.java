8ad4d04c75d93b4f994ec811cb7d49eb15300b04
Tariq Pathan <tariq047@gmail.com>
2023-03-03 13:09:18 +0000

    FactoryHelper javadocs

--- src/sml/provider/IntProviderImpl.java
+++ src/sml/provider/IntProviderImpl.java
@@ -1,5 +1,11 @@
 package sml.provider;
 
+/**
+ * Provides a way to convert the String to Integer.
+ * Autowired into the InstructionFactory, set in ./resources/beans.xml
+ *
+ * @author Tariq Pathan
+ */
 public class IntProviderImpl implements IntProvider{
 
     @Override
--- src/sml/provider/RegisterNameProviderImpl.java
+++ src/sml/provider/RegisterNameProviderImpl.java
@@ -3,6 +3,12 @@ package sml.provider;
 import sml.RegisterName;
 import sml.Registers;
 
+/**
+ * Provides a way to convert the String to RegisterName.
+ * Autowired into the InstructionFactory, set in ./resources/beans.xml
+ *
+ * @author Tariq Pathan
+ */
 public class RegisterNameProviderImpl implements RegisterNameProvider {
 
     @Override
