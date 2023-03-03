3caa0acdcb5eb842547a9226656c9768e125e22c
Tariq Pathan <tariq047@gmail.com>
2023-03-03 18:02:09 +0000

    Added: RegistersTest
    Testing register equals method and toString

--- test/sml/RegistersTest.java
+++ test/sml/RegistersTest.java
@@ -1,26 +1,68 @@
 package sml;
 
-import org.junit.jupiter.api.*;
-import static sml.Registers.Register;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static sml.Registers.Register.*;
 
 class RegistersTest {
   private Machine machine;
-  private Registers r;
-  // TODO: Test the equals and hashcode methods
+  private Registers registers;
 
   @BeforeEach
   void setUp() {
-    r = new Registers();
+    machine = new Machine(new Registers());
+    registers = machine.getRegisters();
   }
 
   @AfterEach
   void tearDown() {
     machine = null;
-  }
-  @Test
-  void setValid() {
-    return;
+    registers = null;
   }
 
+  @Test
+  void equalsValid() {
+    Assertions.assertTrue(registers.equals(registers));
+  }
+
+  @Test
+  void equalsValidTwo() {
+    Machine machineTwo = new Machine(new Registers());
+    Registers registersTwo = machineTwo.getRegisters();
+    Assertions.assertTrue(registersTwo.equals(registers));
+    Assertions.assertTrue(registers.equals(registersTwo));
+  }
+
+  @Test
+  void equalsValidThree() {
+    Machine machineTwo = new Machine(new Registers());
+    Registers registersTwo = machineTwo.getRegisters();
+    registers.set(EAX, 3);
+    registersTwo.set(EAX, 3);
+    Assertions.assertTrue(registersTwo.equals(registers));
+    Assertions.assertTrue(registers.equals(registersTwo));
+  }
+
+  @Test
+  void equalsValidFour() {
+    Machine machineTwo = new Machine(new Registers());
+    Registers registersTwo = machineTwo.getRegisters();
+    registers.set(EAX, 3);
+    registersTwo.set(ECX, 3);
+    Assertions.assertFalse(registersTwo.equals(registers));
+    Assertions.assertFalse(registers.equals(registersTwo));
+  }
+
+  @Test
+  void toStringValid() {
+    registers.set(EAX, 1);
+    registers.set(EDI, 399);
+    registers.set(EBP, 5);
+    String expected = "[EAX = 1, EBX = 0, ECX = 0, EDX = 0, ESP = 0, " +
+            "EBP = 5, ESI = 0, EDI = 399]";
+    Assertions.assertEquals(expected, registers.toString());
+  }
 }
